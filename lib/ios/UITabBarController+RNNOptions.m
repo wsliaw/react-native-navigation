3f7e1fe38af561242bf3f9057612e0da45787fad
wilson <wilson@waveo.com>
2019-01-08 18:35:09 +0800

    fix self.tabBar.hidden value
    Signed-off-by: wilson <wilson@waveo.com>

--- lib/ios/UITabBarController+RNNOptions.m
+++ lib/ios/UITabBarController+RNNOptions.m
@@ -32,18 +32,46 @@
 }
 
 - (void)rnn_setTabBarVisible:(BOOL)visible animated:(BOOL)animated {
-    CGRect nextFrame = self.tabBar.frame;
-    nextFrame.origin.y = UIScreen.mainScreen.bounds.size.height - (visible ? self.tabBar.frame.size.height : 0);
-    
-    [UIView animateWithDuration: (animated ? 0.15 : 0)
-                          delay: 0
-                        options: (visible ? UIViewAnimationOptionCurveEaseOut : UIViewAnimationOptionCurveEaseIn)
-                     animations:^()
-     {
-         [self.tabBar setFrame:nextFrame];
-     }
-                     completion:^(BOOL finished)
-     {}];
+    const CGRect tabBarFrame = self.tabBar.frame;
+	const CGRect tabBarVisibleFrame = CGRectMake(tabBarFrame.origin.x,
+												 self.view.frame.size.height - tabBarFrame.size.height,
+												 tabBarFrame.size.width,
+												 tabBarFrame.size.height);
+	const CGRect tabBarHiddenFrame = CGRectMake(tabBarFrame.origin.x,
+												self.view.frame.size.height,
+												tabBarFrame.size.width,
+												tabBarFrame.size.height);
+	if (!animated) {
+		self.tabBar.hidden = !visible;
+		self.tabBar.frame = visible ? tabBarVisibleFrame : tabBarHiddenFrame;
+		return;
+	}
+	static const CGFloat animationDuration = 0.15;
+
+	if (visible) {
+		self.tabBar.hidden = NO;
+		[UIView animateWithDuration: animationDuration
+							  delay: 0
+							options: UIViewAnimationOptionCurveEaseOut
+						 animations:^()
+		 {
+			 self.tabBar.frame = tabBarVisibleFrame;
+		 }
+						 completion:^(BOOL finished)
+		 {}];
+	} else {
+		[UIView animateWithDuration: animationDuration
+							  delay: 0
+							options: UIViewAnimationOptionCurveEaseIn
+						 animations:^()
+		 {
+			 self.tabBar.frame = tabBarHiddenFrame;
+		 }
+						 completion:^(BOOL finished)
+		 {
+			 self.tabBar.hidden = YES;
+		 }];
+	}
 }
 
 @end
